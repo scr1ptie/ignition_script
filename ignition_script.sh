c92d6ab33e229e40587dabc505d85490c5af7deb
Scriptie <33485136+scr1ptie@users.noreply.github.com>
2021-03-06 17:38:54 +0530

    Update ignition_script.sh

--- ignition_script.sh
+++ ignition_script.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
+# Testing if root...
+if [ $UID -ne 0 ]
+then
+	RED "You must run this script as root!" && echo
+	exit
+fi
+
 # Install sublime Text 3 
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt-get install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install sublime-text
+
+# install nmap
+sudo apt-get install -y nmap
+
+# install wireshark
+sudo apt-get install -y wireshark
+
 
